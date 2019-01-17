026181a9f441de0f5499463d04e562df96d82d41
20zinnm <meyerzinn@gmail.com>
2019-01-17 09:16:51 -0600

    better build script for cross-s-compilation

---
+++ README.md
@@ -0,0 +1,6 @@
+# SMasteroids
+
+> A game in honor of the St. Mark's science faculty and the Winn Science Building.
+
+## Compiling
+
--- build_mac.sh
+++
@@ -1,2 +0,0 @@
-packr
-go build -ldflags "-X gitlab.com/meyerzinn/smasteroids/smasteroids.version=$(git describe)" -o mac/SMasteroids.app/Contents/MacOS/smasteroids gitlab.com/meyerzinn/smasteroids
