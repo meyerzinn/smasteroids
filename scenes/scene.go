1400aed735eee3ddad6125427fa189f170335dfb
20zinnm <meyerzinn@gmail.com>
2019-01-29 08:43:52 -0600

    Add documentation to scene.go

--- scenes/scene.go
+++ scenes/scene.go
@@ -9,6 +9,7 @@ var (
 	// current represents the currently active scene which is rendered.
 	current Scene
 
+	// CanvasBounds represents the dimensions of the canvas (also used for physics).
 	CanvasBounds = pixel.R(-1920/2, -1080/2, 1920/2, 1080/2)
 )
 
@@ -18,16 +19,19 @@ type Scene interface {
 	Render(win *pixelgl.Window)
 }
 
+// DestroyableScene represents a scene that must be explicitly disposed of.
 type DestroyableScene interface {
 	Scene
 	Destroy()
 }
 
+// Draw draws a canvas to the window, properly scaling and moving the canvas.
 func Draw(win *pixelgl.Window, canvas *pixelgl.Canvas) {
 	win.SetMatrix(pixel.IM.Scaled(pixel.ZV, win.Bounds().W()/canvas.Bounds().W()).Moved(win.Bounds().Center()))
 	canvas.Draw(win, pixel.IM.Moved(canvas.Bounds().Center()))
 }
 
+// TransitionTo changes the currently active scene, destroying the previous scene if necessary.
 func TransitionTo(o Scene) {
 	if current != nil {
 		if d, ok := o.(DestroyableScene); ok {
@@ -37,6 +41,7 @@ func TransitionTo(o Scene) {
 	current = o
 }
 
+// Render calls the render method of the currently active scene if it exists.
 func Render(win *pixelgl.Window) {
 	if current != nil {
 		current.Render(win)
